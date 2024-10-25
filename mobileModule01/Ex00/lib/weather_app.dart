542e42f5d0b1c75dba4b165967034f66f5fe48d9
Ergun Sen <ersen@k2m12s06.42kocaeli.com.tr>
2024-10-25 14:07:45 +0300

    m01_ex01

--- mobileModule01/Ex00/lib/weather_app.dart
+++ mobileModule01/Ex00/lib/weather_app.dart
@@ -1,23 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: TabBarExample(),
     );
   }
 }
 
 class TabBarExample extends StatelessWidget {
+  const TabBarExample({super.key});
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 3, // Sekme sayısını belirtir
+      length: 3,
       child: Scaffold(
         appBar: AppBar(
           title: const Text(''),
@@ -29,14 +33,17 @@ class TabBarExample extends StatelessWidget {
             Center(child: Text("Weekly")),
           ],
         ),
-        bottomNavigationBar: const TabBar(
-          tabs: [
-            Tab(icon: Icon(Icons.calendar_today), text: "Currently"),
-            Tab(icon: Icon(Icons.calendar_view_day), text: "Today"),
-            Tab(icon: Icon(Icons.calendar_view_week), text: "Weekly"),
-          ],
-          labelColor: Colors.blue,
-          unselectedLabelColor: Colors.grey,
+        bottomNavigationBar: const BottomAppBar(
+          child: TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.calendar_today), text: "Currently"),
+              Tab(icon: Icon(Icons.calendar_view_day), text: "Today"),
+              Tab(icon: Icon(Icons.calendar_view_week), text: "Weekly"),
+            ],
+            labelColor: Colors.green,
+            unselectedLabelColor: Colors.black,
+            indicatorColor: Colors.green,
+          ),
         ),
       ),
     );

658c88eaa2723afdfcfb32075b442271a5c1e534
Ergun Sen <ersen@k2m14s03.42kocaeli.com.tr>
2024-11-13 14:27:11 +0300

    m02ex01 added

---
+++ mobileModule02/Ex00/lib/weather_app.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: TabBarExample(),
+    );
+  }
+}
+
+class TabBarExample extends StatefulWidget {
+  const TabBarExample({super.key});
+
+  @override
+  TabBarExamplesState createState() => TabBarExamplesState();
+}
+
+class TabBarExamplesState extends State<TabBarExample> {
+  final TextEditingController _controller = TextEditingController();
+  String location = "Konum alınamadı";
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+  }
+
+  Future<void> _getCurrentLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Konum servisinin etkin olup olmadığını kontrol edin
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Konum servisi devre dışı ise kullanıcıya bildirin
+      setState(() {
+        location = "Konum servisi devre dışı.";
+      });
+      return;
+    }
+
+    // Konum izinlerini kontrol edin
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        setState(() {
+          location = "Konum izni reddedildi.";
+        });
+        return;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      setState(() {
+        location = "Geolocation is not available, please enable it in your App settings";
+      });
+      return;
+    }
+
+    // Konum bilgilerini alın
+    Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high,
+    );
+
+    setState(() {
+      location = "Lat: ${position.latitude}, Lng: ${position.longitude}";
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Row(
+            children: [
+                Expanded(
+                  child: TextField(
+                    controller: _controller,
+                    decoration: InputDecoration(
+                      hintText: 'Search...',
+                      prefixIcon: const Icon(Icons.search),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        borderSide: BorderSide.none,
+                      ),
+                      filled: true,
+                      fillColor: Colors.grey[200],
+                    ),
+                    onSubmitted: (_) => _getCurrentLocation(),
+                  ),
+                ),
+                IconButton(
+                  icon: const Icon(Icons.location_searching_sharp),
+                  onPressed: _getCurrentLocation,
+                ),
+              ],
+          ),
+          backgroundColor: Colors.blue,
+        ),
+        body: TabBarView(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text("Currently"),
+                Text(location),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text("Today"),
+                Text(location),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text("Weekly"),
+                Text(location),
+              ],
+            ),
+          ],
+        ),
+        bottomNavigationBar: const BottomAppBar(
+          child: TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.calendar_today), text: "Currently"),
+              Tab(icon: Icon(Icons.calendar_view_day), text: "Today"),
+              Tab(icon: Icon(Icons.calendar_view_week), text: "Weekly"),
+            ],
+            labelColor: Colors.blue,
+            unselectedLabelColor: Colors.black,
+            indicatorColor: Colors.blue,
+          ),
+        ),
+      ),
+    );
+  }
+}
